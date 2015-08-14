f63fe6ded9a300bcca6cc8bb4471987adad4f86f
mitogh <mitogh@gmail.com>
2015-08-14 15:48:04 -0500

    Add install script to install bower and npm packages
    Creation of script to install the:
    - node dependencies
    - bower dependencias
    - composer dependencies (commented)
    
    The composer dependencies has been commented because the current theme
    does not have any dependency from composer.

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+clear
+
+echo ""
+echo "Hi welcome, before install the required files please make sure you have installed"
+echo "
+ - node: https://nodejs.org/download/
+ - bower: http://bower.io/#install-bower
+"
+ # - composer: https://getcomposer.org/doc/00-intro.md#installation-linux-unix-osx
+
+bye(){
+  echo ""
+  echo "Thanks! Have a great day."
+  echo ""
+}
+
+install(){
+  echo ""
+  echo "==========================================================================="
+  echo "                 Installing Packages from Node - By Moxie                  "
+  echo "==========================================================================="
+  echo "Running: npm install, wait, please."
+  npm install
+  echo ""
+  echo "==========================================================================="
+  echo "                Installing Packages from Bower - By Moxie                  "
+  echo "==========================================================================="
+  echo "Running: bower install, wait, please."
+  bower install
+  # echo ""
+  # echo "==========================================================================="
+  # echo "               Installing Packages from Composer - By Moxie                "
+  # echo "==========================================================================="
+  # echo "Running: composer install && composer update, wait, please."
+  # composer install && composer update
+  echo "Installation complete, thank you!"
+  bye
+}
+
+
+echo "Are you ready to continue (yes / no): "
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) install; break;;
+        No ) bye; exit;;
+    esac
+done
