f8541948d755e94f817ea8cd7f8a149e337e3b21
mitogh <mitogh@gmail.com>
2015-10-05 13:05:02 -0500

    Move constants and dependencies into a custom directory

--- config/dependencies.php
+++ config/dependencies.php
@@ -5,13 +5,12 @@ if( ! function_exists('load_dependencies') ){
 		 * Including Theme Hook Alliance
 		 * https://github.com/zamoose/themehookalliance.
 		 */
-		include( 'library/vendors/tha-theme-hooks/tha-theme-hooks.php' );
+		include BASE_THEME_PATH . '/library/vendors/tha-theme-hooks/tha-theme-hooks.php';
 		/**
 		 * Custom template tags for this theme.
 		 */
 		require BASE_THEME_PATH . '/library/vendors/template-tags.php';
 
-		include 'library/class-assets.php';
+		include BASE_THEME_PATH . '/library/class-assets.php';
 	}
 }
-
