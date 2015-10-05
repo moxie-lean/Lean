f65e37c9f8bc3266e1f9b0dff79791bed8b733d0
mitogh <mitogh@gmail.com>
2015-10-05 13:05:32 -0500

    Load dependencies and constans

--- config.php
+++ config.php
@@ -8,4 +8,14 @@ if( ! defined('BASE_THEME_PATH') ){
 	define('BASE_THEME_PATH', get_template_directory() );
 }
 
-include 'dependencies.php';
+/**
+ * Dependencies from other files like external classes from a composer directory
+ * or something different like a custom class to load into the project.
+ */
+include BASE_THEME_PATH . '/config/dependencies.php';
+
+/**
+ * Specific constants to the project can be store in this file, different things
+ * like ACF fields.
+ */
+include BASE_THEME_PATH . '/config/constans.php';
