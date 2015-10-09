52d3f2b5e16cbd9147ccd5bb9ea38a4bf4300838
mitogh <mitogh@gmail.com>
2015-10-09 15:44:40 -0500

    DEfine a constant for the text domain

--- config/constants.php
+++ config/constants.php
@@ -1,4 +1,11 @@
 <?php
 /**
  * File to save storages related to the specifc of the project.
+ * @package Lean
  */
+
+// Text domain of the translated text.
+if ( ! defined( 'TRANSLATED_TEXT_DOMAIN' ) ) {
+	define( 'TRANSLATED_TEXT_DOMAIN', 'lean' );
+}
+
