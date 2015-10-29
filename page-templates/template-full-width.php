60c227aff99b41c95a74be01ee6266ac4652c3b7
mitogh <mitogh@gmail.com>
2015-10-28 22:06:31 -0600

    Update indentation level

--- page-templates/template-full-width.php
+++ page-templates/template-full-width.php
@@ -17,7 +17,7 @@ get_header(); ?>
 				<?php
 				while ( have_posts() ) : the_post();
 
-								get_template_part( 'partials/content', 'page' );
+					get_template_part( 'partials/content', 'page' );
 
 					/**
 					 * If comments are open or we have at least one comment,
