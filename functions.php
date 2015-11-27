3c4374fb985db07db4eea1557bd53cd417d4f97b
mitogh <mitogh@gmail.com>
2015-11-27 11:52:40 -0600

    Remove unused comments from the functions file

--- functions.php
+++ functions.php
@@ -1,40 +1,20 @@
 <?php
 /**
- * Lean functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package Lean
- * @since 1.0.0
- */
-
-use lean\inc as inc;
-
-/**
  * Sets up theme defaults and registers support for various WordPress features.
  *
  * Note that this function is hooked into the after_setup_theme hook, which
  * runs before the init hook.
+ *
+ * @since 1.0.0
  */
 add_action( 'after_setup_theme', function(){
 	include 'config.php';
-
-	/**
-	 * Set the content width based on the theme's design and stylesheet
-	 * (unit based on pixels).
-	 */
-	if ( ! isset( $content_width ) ) {
-		$content_width = 640;
-	}
-
+	Lean\load_dependencies();
 	// Make theme available for translation.
 	load_theme_textdomain( TRANSLATED_TEXT_DOMAIN , FULL_THEME_PATH . '/config/languages' );
-
-	// Enable support for Post Thumbnails on posts and pages.
 	add_theme_support( 'post-thumbnails' );
 	// Add support for all the hooks from tha_hooks.
 	add_theme_support( 'tha_hooks', array( 'all' ) );
-
 	// Add Editor Style for adequate styling in text editor.
 	if ( defined( 'WP_DEBUG' ) && true === WP_DEBUG ) {
 		add_editor_style( EDITOR_STYLESHEET_UNMINIFIED );
@@ -44,13 +24,10 @@ add_action( 'after_setup_theme', function(){
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menu( 'primary-navigation', __( 'Primary Menu', 'lean' ) );
-
-	lean\load_dependencies();
-
 	$args = array(
 		'css_version' => false,
 		'js_version' => time(),
 	);
-	$assets = new inc\Theme_Assets( $args );
+	$assets = new Lean\Inc\Assets( $args );
 	$assets->load();
 });
