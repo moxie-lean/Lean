d2c0d6b15563c183c129322f69e7ba061b82251b
mitogh <mitogh@gmail.com>
2015-11-27 11:55:22 -0600

    Remove not necesary test

--- config/dependencies.php
+++ config/dependencies.php
@@ -1,39 +1,25 @@
-<?php
+<?php namespace Lean;
+
 /**
- * Theme dependencies
- *
+ * Load theme dependencies.
  * @package Lean
- * @subpackage config
  * @since 1.0.0
  */
-
-namespace lean;
-
-// Exit if this fiel is loaded directly.
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
-
-if ( ! function_exists( 'load_dependencies' ) ) {
-	/**
-	 * Load theme dependencies.
+function load_dependencies() {
+	/*
+	 * Including Theme Hook Alliance
+	 *
+	 * @linkg https://github.com/zamoose/themehookalliance.
+	 *
+	 * Use add_theme_support( 'tha_hooks', array('all') ) to add support.
 	 */
-	function load_dependencies() {
-		/*
-		 * Including Theme Hook Alliance
-		 *
-		 * @linkg https://github.com/zamoose/themehookalliance.
-		 *
-		 * Use add_theme_support( 'tha_hooks', array('all') ) to add support.
-		 */
-		require COMPOSER_DIR . '/zamoose/themehookalliance/tha-theme-hooks.php';
+	require COMPOSER_DIR . '/zamoose/themehookalliance/tha-theme-hooks.php';
 
-		/*
-		 * Custom template tags for this theme.
-		 */
-		require INC_DIR . '/class-theme-assets.php';
-		require INC_DIR . '/helpers/loader.php';
-		require INC_DIR . '/helpers/template-tags.php';
-		require INC_DIR . '/helpers/comment.php';
-	}
+	/*
+	 * Custom template tags for this theme.
+	 */
+	require INC_DIR . '/class-theme-assets.php';
+	require INC_DIR . '/helpers/loader.php';
+	require INC_DIR . '/helpers/template-tags.php';
+	require INC_DIR . '/helpers/comment.php';
 }
