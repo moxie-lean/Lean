173bd4caa5a426daee092174e6bf8fda0272e31b
mitogh <mitogh@gmail.com>
2016-01-02 21:16:17 -0600

    Remomove essentials

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -1,22 +1,5 @@
 'use strict';
+var $ = require('jQuery');
 
-window.Leean = {};
-window.Leean.Behaviors = {};
-
-$(document).ready(function() {
-  Essential.loadBehaviors({
-    context: document,
-    application: Leean.Behaviors,
-  });
-});
-
-Leean.Behaviors.Base = Essential.Behavior.extend({
-
-  init: function() {
-    this.$el = $(this.el);
-    if (this.initialize) {
-      this.initialize();
-    }
-  },
-
+$(document).ready(function(){
 });
