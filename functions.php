7d795053bb21e2d5864e3793f2c178b34b5a131e
mitogh <mitogh@gmail.com>
2016-01-24 10:26:29 -0600

    Split actions on the functions file in order to be more modular
    This apprach follows the single responsibility principle where every
    module should be responsibility for a single part of the functionality
    so we can remove a certain action just from the normal flow.
    
    Also defines the action where the constant and files are loaded as the
    first action with lower priority in order to be executed before any
    other action after, since all other action has the default priority of
    10.

--- functions.php
+++ functions.php
@@ -11,22 +11,50 @@
 use Leean\Config;
 use Leean\Assets;
 
+/**
+ * Action to load the configuration file and define constant values, this action
+ * has priority of 1 in order to be executed before any other action using the
+ * defined constant values on the config.php file.
+ */
 add_action( 'after_setup_theme', function(){
 	include 'config.php';
-	// Make theme available for translation.
 	load_theme_textdomain( _TEXT_DOMAIN_ , _THEME_PATH_ . '/languages' );
+}, 1);
+
+/**
+ * Action created to add theme supports or specific of the theme.
+ */
+add_action( 'after_setup_theme', function(){
 	add_theme_support( 'post-thumbnails' );
-	// Add Editor Style for adequate styling in text editor.
+});
+
+/**
+ * Action that adds the styles from the theme to the editor in order to see the
+ * same styles from the front end in the dashboard on the editor section.
+ */
+add_action( 'after_setup_theme', function(){
 	if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
 		add_editor_style( _THEME_PATH_ . '/assets/css/style.css' );
 	} else {
 		add_editor_style( _THEME_PATH_ . '/assets/css/style-min.css' );
 	}
-	// This theme uses wp_nav_menu() in one location.
+});
+
+/**
+ * Action that is responsable for register the menus used on the theme.
+ */
+add_action( 'after_setup_theme', function(){
 	register_nav_menu(
 		'primary-navigation',
 		__( 'Primary Menu', _TEXT_DOMAIN_ )
 	);
+});
+
+/**
+ * This actions register the assets and some configuration about the assets
+ * in order to load the CSS and JS files into the theme.
+ */
+add_action( 'after_setup_theme', function(){
 	$args = [
 		'css_version' => false,
 		'js_version' => time(),
